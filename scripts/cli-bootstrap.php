0daf3a9cc82cb1a5831f677eb3649cf54c93a6e1
Nam Nguyen <namnd86@gmail.com>
2015-01-30 16:19:00 +1100

    email invoice

--- scripts/cli-bootstrap.php
+++ scripts/cli-bootstrap.php
@@ -24,6 +24,11 @@ $config = include __DIR__ . "/../app/config/config.php";
 include __DIR__ . "/../app/config/loader.php";
 
 /**
+ * Read auto-loader
+ */
+include __DIR__ . "/../app/config/loader.php";
+
+/**
  * Read services
  */
 include __DIR__ . "/../app/config/services.php";
