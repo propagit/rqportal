9cce8081cc5fbc281941551a9e85284c2cbf0442
Nam Nguyen <namnguyen@Nams-MBP.gateway>
2015-02-04 09:47:32 +1100

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -5,7 +5,7 @@ return new \Phalcon\Config(array(
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
         'username'    => 'root',
-        'password'    => 'root',
+        'password'    => 'password',
         'dbname'      => 'rqportal',
     ),
     'application' => array(
