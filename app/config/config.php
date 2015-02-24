1cc72dae8e25454641ee8035bad45b0534c3fdc5
Nam Nguyen <namnguyen@Nams-MBP.gateway>
2015-02-24 15:34:43 +1100

    update

--- app/config/config.php
+++ app/config/config.php
@@ -16,8 +16,8 @@ return new \Phalcon\Config(array(
         'libraryDir'     => __DIR__ . '/../../app/library/',
         'formsDir'       => __DIR__ . '/../../app/forms/',
         'cacheDir'       => __DIR__ . '/../../app/cache/',
-        'baseUri'        => '/',
-        'publicUrl'      => 'http://rqportal.com/'
+        'baseUri'        => '/rqportal/',
+        'publicUrl'      => 'http://nam.dev/rqportal/'
     ),
     'mail' => array(
         'toName'    => 'Team',
