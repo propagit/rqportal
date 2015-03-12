1527c54c6e742cc36fc0b095b66332b0a2bae110
Kaushtuv <kaushtuvgurung@gmail.com>
2015-03-12 12:59:23 +1100

    pdf memory

--- app/controllers/BillingController.php
+++ app/controllers/BillingController.php
@@ -75,7 +75,6 @@ class BillingController extends ControllerBase
 
     private function _generatePdf($id)
     {
-		#ini_set("memory_limit","128M");
         $file = __DIR__ . '/../../public/files/invoice' . $id . '.pdf';
         if (!file_exists($file)){
 			 unlink($file);
