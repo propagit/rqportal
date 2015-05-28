b9f8ad54ada64c512e93c382c872c6f7823455a3
Kaushtuv <kaushtuvgurung@gmail.com>
2015-05-28 17:36:51 +1000

    API

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
-error_reporting(1);
+error_reporting(-1);
 
 # Use Loader() to autoload our model
 $loader = new \Phalcon\Loader();
