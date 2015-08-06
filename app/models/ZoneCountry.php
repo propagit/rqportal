188d63e00f6880258b67a31dd0eb3c2321b96516
Kaushtuv <kaushtuvgurung@gmail.com>
2015-08-06 10:05:26 +1000

    Postcode Pool Bug

--- app/models/ZoneCountry.php
+++ app/models/ZoneCountry.php
@@ -107,15 +107,14 @@ class ZoneCountry extends BaseModel
     public function generatePool()
     {
         $local = ZoneLocal::findFirst("id = " . $this->local_id);
-		$result = $this->db->query("SELECT p.postcode FROM Postcodes p WHERE postcode_dist($local->postcode, p.postcode) <= $this->distance");
-		/*$query = $this->modelsManager->createQuery("SELECT p.postcode FROM Postcodes p WHERE postcode_dist($local->postcode, p.postcode) <= $this->distance");
-		$result = $query->execute();*/
+
+		$query = $this->modelsManager->createQuery("SELECT p.postcode FROM Postcodes p WHERE postcode_dist($local->postcode, p.postcode) <= $this->distance");
+		$result = $query->execute();
 		
         $pool = array();
-		$result->setFetchMode(Phalcon\Db::FETCH_OBJ);
-        while($postcode = $result->fetch()) {
-            $pool[] = $postcode->postcode;
-        }
+		foreach($result as $postcode){
+			$pool[] = $postcode->postcode;
+		}
         $this->pool_local = $local->pool;
         $this->pool_country = json_encode($pool);
         $this->save();
