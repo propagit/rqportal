b729a61c262f3082b28af73300cf412706cfc752
Nam Nguyen <15213925@students.latrobe.edu.au>
2016-02-01 13:18:40 +1100

    debug

--- app/controllers/BillingajaxController.php
+++ app/controllers/BillingajaxController.php
@@ -62,7 +62,7 @@ class BillingajaxController extends ControllerAjax
             "bind" => $parameters,
             "order" => "id DESC",
             "limit" => $per_page,
-            "offset" => 1 //$offset
+            "offset" => 2 //$offset
         ));
         $results = array();
         foreach($invoices as $invoice)
