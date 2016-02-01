59870175de38fec023f9be9d348faf2654fa10aa
Nam Nguyen <15213925@students.latrobe.edu.au>
2016-02-01 13:33:59 +1100

    debug

--- app/models/Invoice.php
+++ app/models/Invoice.php
@@ -145,7 +145,7 @@ class Invoice extends \Phalcon\Mvc\Model
         $invoice['free'] = $free;
         // $invoice['removals'] = $removals;
         // $invoice['storages'] = $storages;
-        // $invoice['lines'] = json_decode($invoice['lines']);
+        $invoice['lines'] = json_decode($invoice['lines']);
         return $invoice;
     }
 
