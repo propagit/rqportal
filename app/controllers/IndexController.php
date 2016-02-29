1abc02774dfca29e42e2637e60b8e28c47226d9b
Nam Nguyen <15213925@students.latrobe.edu.au>
2016-02-29 14:39:57 +1100

    test

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -10,9 +10,25 @@ class IndexController extends ControllerBase
 
     public function testAction()
     {
-        $zone = ZoneLocal::findFirst(317);
+        /*$zone = ZoneLocal::findFirst(317);
         $pool = $zone->generatePool();
-        print_r($pool); die();
+        print_r($pool); die();*/
+
+        $removal = Removal::findFirst(21061);
+        $from = $removal->from_postcode;
+        $to = $removal->to_postcode;
+        $cc = explode(',', 'namnd86@gmail.com, daniel@propagate.com.au,namndvn@yahoo.com.au');
+        $this->mail->send(
+            array("nam@propagate.com.au" => "Nam Nguyen"),
+            'New Removalist Job',
+            'new_removal',
+            array(
+                'removal' => $removal,
+                'from' => $from,
+                'to' => $to
+            ),
+            $cc
+        );
     }
 }
 
