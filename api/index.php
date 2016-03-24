bd717c507d3af4ee9be91208f374ee670cca1380
Nam Nguyen <15213925@students.latrobe.edu.au>
2016-03-24 16:12:22 +1100

    test

--- api/index.php
+++ api/index.php
@@ -4,6 +4,8 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
+include __DIR__ . "/../vendor/autoload.php";
+
 # Use Loader() to autoload our model
 $loader = new \Phalcon\Loader();
 
